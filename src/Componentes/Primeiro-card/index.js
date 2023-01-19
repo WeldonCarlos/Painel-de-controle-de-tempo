821ba60a7e24bd01d5a3f72b0814dfbd1e5a6237
Weldon Carlos <weldonprogrammer2022@gmail.com>
2023-01-18 22:11:23 -0300

    Component primeiro card

---
+++ src/Componentes/Primeiro-card/index.js
@@ -0,0 +1,54 @@
+import '../Primeiro-card/primeiro-card.css'
+import Perfil from '../../Assets/img/img-perfil.png'
+
+
+const PrimeiroCard = () => {
+
+    return (
+
+        <section >
+
+            <div className='conteiner__pai'>
+
+                <div className='fundo__card'>
+
+                    <div className="frente__card"></div>
+                </div>
+
+                <div className='conteiner__img__descri'>
+
+
+                    <div className='circulo__branco'>
+                        <img className="img__log" src={Perfil} alt="Foto do perfil" />
+
+                    </div>
+
+
+                    <div className='topo__card__info'>
+                        <p>Relatório</p>
+                        <h2>Jeremias Robson</h2>
+                    </div>
+
+
+                    <div className="texto__rodape">
+                        <h3 className='textos__rodape'>Dia</h3>
+                        <h3 className='textos__rodape'>Semana</h3>
+                        <h3 className='textos__rodape'>Mês</h3>
+                    </div>
+
+
+                </div>
+
+
+
+
+            </div>
+
+
+
+        </section>
+    )
+}
+
+
+export default PrimeiroCard
