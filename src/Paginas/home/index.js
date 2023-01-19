ea0d6c7bf8b7f962d9e7f37df3926cc17df0acc9
Weldon Carlos <weldonprogrammer2022@gmail.com>
2023-01-18 22:08:55 -0300

    importei para o componente home

---
+++ src/Paginas/home/index.js
@@ -0,0 +1,17 @@
+import CardPequeno from "../../Componentes/Card-pequeno"
+import PrimeiroCard from "../../Componentes/Primeiro-card"
+
+const Home = () => {
+    return (
+
+        <main>
+            <PrimeiroCard />
+            <CardPequeno/>
+           
+        </main>
+
+    )
+}
+
+
+export default Home
